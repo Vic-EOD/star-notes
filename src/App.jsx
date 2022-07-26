ad58e0256d7762b06bdaab125b7ce3264c4dec07
Victor Martinez <victor.eod2@gmail.com>
2022-07-25 23:13:59 -0700

    add basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App p-8 rounded-xl container flex flex-col justify-center align-center bg-gray-300 w-100 h-100">
       <header>
-        <h1 className="text-2xl">My Stories</h1>
+        <h1 className="text-5xl text-center mb-12">My Stories</h1>
       </header>
       <StoryList stories={stories} />
     </div>
--- src/StoryList.jsx
+++ src/StoryList.jsx
@@ -1,8 +1,37 @@
-import Story from "./Story";
+const Story = ({ title, shortDescription }) => {
+  return (
+    <div className="flex p-4 gap-6 items-center justify-evenly w-6/12 bg-white rounded-xl shadow-md">
+      <h2 className="text-3xl text-red-600 font-semibold">{title}</h2>
+      <p className="align-middle text-lg">{shortDescription}</p>
+      <div className="flex gap-4 justify-self-end">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 hover:cursor-pointer"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 hover:cursor-pointer"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
 
 const StoryList = ({ stories }) => {
   return (
-    <div>
+    <div className="flex flex-col gap-5 justify-center items-center">
       {!stories.length ? (
         <h1>No Stories Found</h1>
       ) : (
@@ -10,7 +39,7 @@ const StoryList = ({ stories }) => {
           return (
             <Story
               title={story.title}
-              shortDescription={story.situation.substring(0, 30)}
+              shortDescription={`${story.situation.substring(0, 30)}...`}
               key={index}
             />
           );
